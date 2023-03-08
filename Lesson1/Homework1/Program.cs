158f3c538ada03dcfac1924675f64f47109462b4
Alexgrin216 <126878501+Alexgrin216@users.noreply.github.com>
2023-03-08 21:14:24 +0700

    Add HW with names for Lesson 1

---
+++ Lesson1/Homework1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+    namespace CodeName
+{
+    class Programm
+    {
+        public static void Main()
+        {
+            string? name;
+            Console.WriteLine("Введите имя");
+            name = Console.ReadLine();
+            Console.WriteLine($"Привет,{name}!");
+        }
+    }
+}
